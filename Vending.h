7b754d6828f051f6158ffdec3a769dd5b3e55726
ianmurungi09-ui <ianmurungi09@gmail.com>
2025-10-11 16:53:18 +0300

    Update Vending.h

--- Vending.h
+++ Vending.h
@@ -1,17 +1,18 @@
 #ifndef VENDING_H
 #define VENDING_H
 
+// Define the number of items in the vending machine (6 items)
 #define ITEMS 6
 
-// Structure to hold item details
+// Structure to represent an item in the vending machine
 typedef struct {
-    char name[20];
-    int price;
-    int stock;
+    char name[20]; // Item name (up to 19 characters)
+    int price;     // Item price in UGX
+    int stock;     // Number of items in stock
 } Item;
 
-// Function prototypes
-void displayMenu(Item items[]);
-void calculateChange(int change, int denominations[], int numDenoms);
+// Function prototypes for vending machine operations
+void displayMenu(Item items[]); // Displays the menu of available items
+void calculateChange(int change, int denominations[], int numDenoms); // Calculates and prints change breakdown
 
 #endif
