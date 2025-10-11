2fa9a75d538279259a36aa59d6fada85c78a8d22
ianmurungi09-ui <ianmurungi09@gmail.com>
2025-10-11 16:59:15 +0300

    Update Vending.c

--- Vending.c
+++ Vending.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "Vending.h"
 
+// Displays the vending machine menu with item details
 void displayMenu(Item items[]) {
+    // Print menu header
     printf("\n--- Vending Machine Menu ---\n");
+    // Loop through all items to display their number, name, price, and stock
     for (int i = 0; i < ITEMS; i++) {
         printf("%d. %s - %d UGX (%d in stock)\n", i + 1, items[i].name, items[i].price, items[i].stock);
     }
+    // Print exit option
     printf("0. Exit\n");
 }
